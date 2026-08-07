abb2121dae2f2168cd07a001c57e37ead1d3dbe4
HenriqueHBM <henriquemadeira1@gmail.com>
2026-08-07 17:10:05 -0300

    Initial commit

---
+++ src/main/java/projeto/OButecoBack_End/OButecoBackEndApplication.java
@@ -0,0 +1,13 @@
+package projeto.OButecoBack_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OButecoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OButecoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/OButecoBack_End/OButecoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.OButecoBack_End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OButecoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
