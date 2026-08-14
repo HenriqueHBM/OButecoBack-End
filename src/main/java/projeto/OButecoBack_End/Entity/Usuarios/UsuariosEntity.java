7ce244e7b3f6fd1fbc9b354a01f7e531468d7137
HenriqueHBM <henriquemadeira1@gmail.com>
2026-08-14 08:19:47 -0300

    feat: realizado conexao com o banco de dados

--- src/main/java/projeto/OButecoBack_End/Entity/Usuarios/UsuariosEntity.java
+++ src/main/java/projeto/OButecoBack_End/Entity/Usuarios/UsuariosEntity.java
@@ -1,4 +1,40 @@
 package projeto.OButecoBack_End.Entity.Usuarios;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "usuarios")
 public class UsuariosEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "usuario", nullable = false, unique = true)
+    private String usuario;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cargo", nullable = false)
+    private CargosEntity cargosEntity;
+
+    @Column(name = "created_at")
+    private Timestamp created_at;
+
+    @Column(name = "updated_at")
+    private Timestamp updated_at;
+
+    @Column(name = "deleted_at")
+    private Timestamp deleted_at;
 }
