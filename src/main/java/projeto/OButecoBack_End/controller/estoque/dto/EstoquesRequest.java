2dcd3899bdf0104a571e438e1589a44bf7aff510
HenriqueHBM <henriquemadeira1@gmail.com>
2026-09-19 18:36:50 -0300

    ajuste no validator de cadastro de estoque

--- src/main/java/projeto/OButecoBack_End/controller/estoque/dto/EstoquesRequest.java
+++ src/main/java/projeto/OButecoBack_End/controller/estoque/dto/EstoquesRequest.java
@@ -6,9 +6,9 @@ public record EstoquesRequest(
         @NotNull(message = "Produto é obrigatório")
         Long fk_id_produto,
 
-        @NotBlank(message = "Quantidade é obrigatória")
+        @NotNull(message = "Quantidade é obrigatória")
         @Positive(message = "Quantidade deve ser maior que zero")
-        double qtdeEstoque,
+        Double qtdeEstoque,
 
         @NotNull(message = "Conversão é obrigatória")
         Long fk_id_conversao,
